cd1469940209d8672330d9bbf40ae80fc5638a4b
Lidia Alonso <l.alonsodiaz@gmail.com>
2020-08-11 17:59:18 +0200

    enabled heroku server

--- script.js
+++ script.js
@@ -1,25 +1,64 @@
-//get Quote from API
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
-async function getQuote(){
-  const proxyUrl= 'https://cors-anywhere.herokuapp.com/'
-   const apiUrl= 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-
-   try{
-
-    const response= await fetch(proxyUrl + apiUrl);
-    const data=await response.json();
-    console.log(data);
-
-
-   }catch (error){
-    getQuote();
-    console.log('whoops, no quote',error);
-   }
-
-
+// Loading Spinner Shown
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
 }
 
+// Remove Loading Spinner
+function complete() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
 
-// On Load  
+// Get Quote From API
+async function getQuote() {
+  loading();
+  // We need to use a Proxy URL to make our API call in order to avoid a CORS error
+  const proxyUrl = 'https://sleepy-scrubland-55732.herokuapp.com/'
+  const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+    // Check if Author field is blank and replace it with 'Unknown'
+    if (data.quoteAuthor === '') {
+      authorText.innerText = 'Unknown';
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+    // Dynamically reduce font size for long quotes
+    if (data.quoteText.length > 120) {
+      quoteText.classList.add('long-quote');
+    } else {
+      quoteText.classList.remove('long-quote');
+    }
+    quoteText.innerText = data.quoteText;
+    // Stop Loading, Show Quote
+    complete();
+  } catch (error) {
+    getQuote();
+  }
+}
 
+// Tweet Quote
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On Load
 getQuote();
